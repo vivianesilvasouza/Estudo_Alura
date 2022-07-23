1d296bcda4d78528faa85bb51aa81d3ddc85ee40
Viviane <vivianedasilvasouza2020@gmail.com>
2022-07-23 15:48:57 -0300

    feat:operadores lógico

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -9,11 +9,27 @@ var tdAltura = paciente.querySelector('.info-altura')
 var altura = tdAltura.textContent
 
 var tdImc = paciente.querySelector('.info-imc')
+var pesoValido = true
+var alturaValida = true
 
-var imc = peso / (altura * altura)
+if (peso <= 0 || peso >= 1000) {
+    console.log('peso inválido!')
+    pesoValido = false
+    tdImc.textContent = 'Peso Inválido'
+}
+if (altura <= 0 || altura >= 4.00) {
+    console.log('altura inválida!')
+    alturaValida = false
+    tdImc.textContent = 'Altura Inválida'
+}
+if (alturaValida && pesoValido) {
+    var imc = peso / (altura * altura)
+    tdImc.textContent = imc
+}
 
-tdImc.textContent = imc
+
+/* tdImc.textContent = imc
 console.log(paciente)
 console.log(peso)
 console.log(altura)
-console.log(imc)
+console.log(imc) */
