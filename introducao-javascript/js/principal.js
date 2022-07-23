6d20311d5897348606813b036c88a988d19f2384
Viviane <vivianedasilvasouza2020@gmail.com>
2022-07-23 15:03:05 -0300

    feat:calculando e exibindo valores

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,2 +1,19 @@
 var titulo = document.querySelector('.titulo')
 titulo.textContent = 'Dr. Aparecida Nutricionista'
+
+var paciente = document.querySelector('#primeiro-paciente')
+var tdPeso = paciente.querySelector('.info-peso')
+var peso = tdPeso.textContent
+
+var tdAltura = paciente.querySelector('.info-altura')
+var altura = tdAltura.textContent
+
+var tdImc = paciente.querySelector('.info-imc')
+
+var imc = peso / (altura * altura)
+
+tdImc.textContent = imc
+console.log(paciente)
+console.log(peso)
+console.log(altura)
+console.log(imc)
